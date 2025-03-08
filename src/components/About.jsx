6a9f2ab8b81ab0ae48c922333d12ca0879d75e09
Barasov-Mehdi <barasovmehdii@gmail.com>
2025-03-08 19:31:54 +0400

    08.03.2025

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import '../scss/About.scss';
 
 function About() {
     return (
-        <div>
-            <h1>About Page</h1>
+        <div className="about-container">
+
+            <h1>Haqqımızda</h1>
+            <p>
+                Peşəkar Mühasib Konsaltinq MMC mühasibatlıq sahəsində təcrübəli və peşəkar əməkdaşların birlikdə ərsəyə gətirdiyi bir layihədir. Şirkətimiz yarandığı tarixdən bu günədək vergi və mühasibatlıq sahəsində çoxlu sayda uğurlara imza atmışdır.
+            </p>
+            <p>
+                Bizim təklif etdiyimiz xidmətlərdən biri sahibkarlıq sahəsində yeni fəaliyyətə başlamış fiziki və ya hüquqi şəxslərə biznes konsaltinq və uçot sahəsində keyfiyyətli dəstək verməkdir. Həmçinin, müştərilərimizin ehtiyaclarına uyğun olaraq özəl layihələr üzərində işləyirik.
+            </p>
+            <p>
+                Son olaraq qeyd etmək istərdik ki, fəaliyyətimiz ilə sizlərə dəstək olmaq bizim işimizin məqsədidir. Hədəfimiz müştərilərimizin mühasibatlıq proseslərini asanlaşdırmaq, hər bir müştəriyə fərdi yanaşma ilə xidmət göstərməkdir.
+            </p>
         </div>
-    )
+    );
 }
 
-export default About
+export default About;
